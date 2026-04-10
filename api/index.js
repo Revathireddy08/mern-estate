bd3454847917065ab55d1246b165b28eec60ff1b
Revathireddy08 <revathireddy355@gmail.com>
2026-04-10 16:21:26 +0530

    fix render deployment issue

--- api/index.js
+++ api/index.js
@@ -50,14 +50,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+
 
 // routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-// fallback route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
